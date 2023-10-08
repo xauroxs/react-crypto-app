4d1f41342b0230cc4d561cd8ee3223f56bbb8d5f
xauroxs <xauroxs@gmail.com>
2023-10-08 21:37:40 +0300

    create not found page

--- src/pages/not-found/not-found.page.tsx
+++ src/pages/not-found/not-found.page.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
 
+import styles from "./not-found.module.scss";
+
+const { notFound, notFound__title, notFound__description } = styles;
+
 const NotFoundPage: FC = () => {
-  return <div></div>;
+  return (
+    <div className={notFound}>
+      <h1 className={notFound__title}>404</h1>
+      <p className={notFound__description}>Sorry, Page Not Found</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
